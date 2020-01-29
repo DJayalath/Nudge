ae18348d1660bd3e2a93be4f7f8a029e957f689e
Dulhan Jayalath <dhjayalath@protonmail.com>
2020-01-29 18:15:31 +0000

    Removed redundant import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_nudge_reminders/task_list.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   runApp(new NudgeApp());
