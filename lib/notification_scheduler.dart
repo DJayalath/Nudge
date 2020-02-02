9a2b4a519b5c0c5d44edd5bedcb035683555ec76
Dulhan Jayalath <dhjayalath@protonmail.com>
2020-02-02 22:37:55 +0000

    Schedule a notification for both the time and early reminder time.

--- lib/notification_scheduler.dart
+++ lib/notification_scheduler.dart
@@ -4,6 +4,8 @@ import 'task.dart';
 
 class NotificationScheduler {
 
+  static const int EARLY_REMINDER_UNIQUE_CONSTANT = 1000;
+
   /// Flutter local notifications plugin instance.
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
@@ -17,35 +19,46 @@ class NotificationScheduler {
 
   static deleteNotification(Task task) async {
     try {
-      await flutterLocalNotificationsPlugin.cancel(task.computeHash());
+      await flutterLocalNotificationsPlugin.cancel(task.id);
+      await flutterLocalNotificationsPlugin.cancel(task.id * EARLY_REMINDER_UNIQUE_CONSTANT);
     } catch (e) {}
   }
 
   static scheduleNotification(Task task) async {
 
-    var hash = task.computeHash();
-
-    try {
-      await flutterLocalNotificationsPlugin.cancel(hash);
-    } catch (e) {}
+    deleteNotification(task);
 
     var message = 'Due now';
-    var time = task.date;
-    if (task.isEarlyReminderSet) {
-      time = time.subtract(task.earlyReminder);
-      var durationInMinutes = task.earlyReminder.inMinutes;
-      message = 'In $durationInMinutes ${(durationInMinutes > 45) ? "hours" : "minutes"}';
-    }
 
     var android = AndroidNotificationDetails('channel id', 'channel NAME', 'CHANNEL DESCRIPTION');
     var iOS = IOSNotificationDetails();
     var platform = NotificationDetails(android, iOS);
+
+    // Schedule main notification.
     await flutterLocalNotificationsPlugin.schedule(
-        hash,
+        task.id,
         task.title,
         message,
-        time,
-        platform, payload: task.title);
+        task.date,
+        platform, payload: task.title
+    );
+
+    // Add another, earlier notification if needed.
+    if (task.isEarlyReminderSet) {
+
+      var time = task.date;
+      time = time.subtract(task.earlyReminder);
+      var durationInMinutes = task.earlyReminder.inMinutes;
+      message = 'In $durationInMinutes ${(durationInMinutes > 45) ? "hours" : "minutes"}';
+
+      await flutterLocalNotificationsPlugin.schedule(
+          task.id * EARLY_REMINDER_UNIQUE_CONSTANT, // TODO: This limits max. unique tasks to 1000 if all have early reminders.
+          task.title,
+          message,
+          time,
+          platform, payload: task.title
+      );
+    }
   }
 
 }
